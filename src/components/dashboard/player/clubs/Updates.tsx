f6a253135bdbce21d8ceea85dd0813aa3914d836
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-07-29 20:01:53 +0100

    Completed the first page of the football club

--- src/components/dashboard/player/clubs/Updates.tsx
+++ src/components/dashboard/player/clubs/Updates.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 const Updates = () => {
-  return (
-    <div>Updates</div>
-  )
-}
+  const [updates, setUpdates] = useState<string[]>(Array(4).fill("Next home match pre camping is going to resume tomorrow, all players will be available except injured ones are out"));
 
-export default Updates
+  return (
+    <div className="flex flex-col rounded-br-[16px] overflow-hidden border-2 border-border-gray shadow-custom-2 items-center bg-white">
+      <div className="bg-primary-2 bg-opacity-[0.96] py-2 w-full">
+        <h2 className="text-white text-lg font-bold py-3 px-5">Updates</h2>
+      </div>
+
+      <div className="flex flex-col gap-2 w-full px-5 py-2">
+        {updates.map((u, i) => (
+          <div
+            key={i}
+            className="flex items-center w-full border  border-border-gray py-1 px-2 rounded-[8px]"
+          >
+            <p className="text-[14px] text-black">{u}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Updates;
