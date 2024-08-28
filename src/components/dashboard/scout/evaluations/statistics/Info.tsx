7e993f4e10bb88d99470f778319560b5f18e5032
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-08-28 13:09:48 +0100

    progress

--- src/components/dashboard/scout/evaluations/statistics/Info.tsx
+++ src/components/dashboard/scout/evaluations/statistics/Info.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image, { StaticImageData } from "next/image";
 
 import PlayerImg from "@/public/images/passport.png";
 import NigFlag from "@/public/images/twemoji_flag-nigeria.png";
+import { convertDateWithSlashes } from "@/src/functions/dateFunctions";
 
 interface iPlayerInfo {
   image: StaticImageData | string;
@@ -19,90 +22,93 @@ interface iPlayerInfo {
 }
 
 const Info = () => {
+  const [info, setInfo] = useState<iPlayerInfo>({
+    image: PlayerImg,
+    country: NigFlag,
+    firstName: "Adams",
+    lastName: "Taylor",
+    jersey: 23,
+    dob: new Date(),
+    role: "Midfielder",
+    height: 160,
+    weight: 69,
+    appearance: 20,
+    foot: "Right",
+  });
+
   return (
-    <div className="w-full shadow-custom rounded-[1rem] py-4 px-5 gap-5 bg-white flex flex-col ">
-      <div className="flex items-center flex-row gap-4">
-        <Image className="w-[124px] h-[124px]" src={PlayerImg} alt={""} />
+    <div className="w-full shadow-custom rounded-[1rem] py-4 px-5 gap-6 bg-white grid grid-cols-[1.5fr_3.5fr]">
+      <div className="flex items-center gap-4">
+        <Image
+          className="size-[7rem]"
+          src={info.image}
+          alt={""}
+          width={110}
+          height={110}
+        />
         <div className="flex flex-col gap-1">
-          <p className="font-lato font-normal text-base">Henry</p>
-          <h2 className="font-lato font-bold text-[20px] leading-[30px]">
-            FAYOMI
+          <p className="text-14-16 text-dark ">{info.firstName}</p>
+          <h2 className="text-16-19 font-bold text-dark text-opacity-[0.92]">
+            {info.lastName}
           </h2>
-          <Image className="w-[24px] h-[24px]" src={NigFlag} alt={""} />
-          <h1 className="font-lato font-medium text-[32px] leading-[30px] text-secondary">
-            #12
-          </h1>
+          <Image
+            className="size-5"
+            src={info.country}
+            alt={""}
+            width={20}
+            height={20}
+          />
+          <h1 className="text-dark font-bold text-28-33">#{info.jersey}</h1>
         </div>
       </div>
 
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-[1rem] flex-row">
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              DATE OF BIRTH
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              17/06/2004
-            </span>
-          </div>
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              POSITION
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              Centre Forward
-            </span>
-          </div>
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              INJURIES
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              Available
-            </span>
-          </div>
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              WEIGHT
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              175 lbs (79kg)
-            </span>
+      <div className="w-full grid grid-cols-3 gap-3">
+        <div className="flex flex-col gap-2">
+          <p className="text-10-12 text-opacity-[0.88] text-dark font-medium">
+            DATE OF BIRTH
+          </p>
+          <div className="border border-border-gray w-full py-1 flex items-center justify-center rounded-md text-14-16 font-medium text-dark">
+            {convertDateWithSlashes(info.dob)}
           </div>
         </div>
-
-        <div className="flex gap-[1rem] flex-row">
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              NATIONALITY
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              Nigerian
-            </span>
+        <div className="flex flex-col gap-2">
+          <p className="text-10-12 text-opacity-[0.88] text-dark font-medium">
+            HEIGHT
+          </p>
+          <div className="border border-border-gray w-full py-1 flex items-center justify-center rounded-md text-14-16 font-medium text-dark">
+            {info.height}cm
           </div>
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              HEIGHT
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              6&apos;2&quot; (188cm)
-            </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-10-12 text-opacity-[0.88] text-dark font-medium">
+            APPEARANCE
+          </p>
+          <div className="border border-border-gray w-full py-1 flex items-center justify-center rounded-md text-14-16 font-medium text-dark">
+            {info.appearance}
           </div>
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              AGE
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              20
-            </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-10-12 text-opacity-[0.88] text-dark font-medium">
+            POSITION
+          </p>
+          <div className="border border-border-gray w-full py-1 flex items-center justify-center rounded-md text-14-16 font-medium text-dark">
+            {info.role}
           </div>
-          <div>
-            <p className="text-[10px] font-lato leading-[12px] font-normal mb-2">
-              DOMINANT FOOT
-            </p>
-            <span className="border border-[#4BBAC1] w-[150px] h-[30px] flex items-center justify-center rounded-md font-semibold text-[14px] leading-[16px] font-lato">
-              Right
-            </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-10-12 text-opacity-[0.88] text-dark font-medium">
+            WEIGHT
+          </p>
+          <div className="border border-border-gray w-full py-1 flex items-center justify-center rounded-md text-14-16 font-medium text-dark">
+            {info.weight}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-10-12 text-opacity-[0.88] text-dark font-medium">
+            PREFERRED FOOT
+          </p>
+          <div className="border border-border-gray w-full py-1 flex items-center justify-center rounded-md text-14-16 font-medium text-dark">
+            {info.foot}
           </div>
         </div>
       </div>
